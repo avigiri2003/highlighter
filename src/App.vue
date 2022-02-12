9807924ca93b9fe178aa2469daeb74ce9d253f16
Avinash J Giri <avingiri@in.ibm.com>
2022-02-12 14:01:36 +0530

    firstversion

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,33 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+        <img src="./assets/logo.png">
+        <div>
+            Search: <input type="text" v-model="query" placeholder="Search" />
+        </div>
+        <br />
+        <div v-html="highlight()"></div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data() {
+            return {
+                query: "",
+                content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse dignissim leo massa, sed aliquet leo posuere ut. Curabitur malesuada accumsan erat, id varius eros tincidunt quis. Vivamus lobortis, odio at fermentum efficitur, risus est tincidunt nisl, eget condimentum tellus dui vitae nibh. Donec id lorem condimentum, porttitor augue in, fermentum leo. Morbi condimentum, nunc ut malesuada placerat, sem nulla condimentum purus, iaculis tristique metus diam lobortis enim. Suspendisse potenti. Quisque urna magna, porta eget erat ac, pharetra vehicula erat. Suspendisse sed nisi ex."
+            }
+        },
+        methods: {
+            highlight() {
+                if(!this.query) {
+                    return this.content;
+                }
+                return this.content.replace(new RegExp(this.query, "gi"), match => {
+                    return '<span class="highlightText">' + match + '</span>';
+                });
+            }
+        }
 }
 </script>
 
@@ -25,4 +40,7 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+.highlightText {
+        background: yellow;
+    }
 </style>
